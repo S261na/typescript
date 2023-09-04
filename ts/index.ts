aee2c1af69dfa06cbbec5f2afff7fc92a457856b
S261na <sabinastalbekova258@gmail.com>
2023-09-04 18:37:53 +0600

    first commit

---
+++ ts/index.ts
@@ -0,0 +1,55 @@
+//  1 task
+//  let a: number;
+// let b: string;
+//  let c: boolean;
+
+//  2 task
+//  interface IAreaFormula {
+//   A: any; //   p: number;
+//   r: number; // }
+// enum Formula1 { //   p = 3.14,
+// } // function calculateArea(radius: number) {
+//   let A = Formula1.p * radius ** 2; //   return A;
+// } // console.log(calculateArea(5));
+
+//  function calculateArea(radius: number): number {
+//   const pi = 3.14;
+//   const area = pi * radius ** 2;
+//   return area; }
+// console.log(calculateArea(5));
+//
+// function calculateDensity(mass: number, volume: number): number {   const density = mass / volume;
+//   return density; }
+// console.log(calculateDensity(100, 50));
+// task 3
+// type Product = {
+//   id: number;
+//   title: string;
+//   description: string;
+//   price: number;
+//   colors: string[];
+// };
+// const products: Product[] = [
+//   {
+//     id: 1,
+//     title: "Сабина",
+//     description: "студент",
+//     price: 234567654,
+//     colors: ["white"],
+//   },
+//   {
+//     id: 2,
+//     title: "Маржан",
+//     description: "student",
+//     price: 345687654,
+//     colors: ["yellow"],
+//   },
+//   {
+//     id: 3,
+//     title: "Аман",
+//     description: "мощь",
+//     price: 1234723654,
+//     colors: ["green"],
+//   },
+// ];
+// console.log(products);
